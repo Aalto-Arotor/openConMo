a8912df3de71a4f14771c8636607afc8c304048f
Elmo Laine <elmo.laine15@gmail.com>
2025-06-06 15:46:45 +0300

    Update conf.py and pyproject.toml

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@ sys.path.insert(0, os.path.abspath('../src'))
 project = 'openConMo'
 copyright = '2025, Sampo Laine, Sampo Haikonen, Aleksanteri Hämäläinen, Elmo Laine'
 author = 'Sampo Laine, Sampo Haikonen, Aleksanteri Hämäläinen, Elmo Laine'
-release = '1.0.0'
+release = '0.0.1'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
